fedd8f741fc2e5eff169e3b1fb4a64341ddea518
AbhilashGopidas <agopidas@wellthie.com>
2020-05-22 15:22:43 -0400

    Update the code base to have the user data passed from S3

--- webserver.tf
+++ webserver.tf
@@ -107,8 +107,4 @@ resource "aws_security_group" "elb" {
   }
 }
 
-output "this_lb_dns_name" {
-  description = "The DNS name of the load balancer."
-  value       = concat(aws_elb.web_elb.this.*.dns_name, [""])[0]
-}
 
