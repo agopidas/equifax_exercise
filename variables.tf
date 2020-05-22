182623f8206d5340953ad9eba354de02b0d44340
AbhilashGopidas <agopidas@wellthie.com>
2020-05-22 02:28:56 -0400

    update the terraform code to create a VPC with Public and Private subnet with ports 22 and 80 exposed

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "Region for the VPC"
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default = "10.0.0.0/21"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for the private subnet"
+  default = "10.0.2.0/24"
+}
+
+variable "ami" {
+  description = "Amazon Linux AMI"
+  default = {
+        us-east-1 = "ami-0c2a1acae6667e438"
+    }
+}
+
+variable "key_path" {
+  description = "SSH Public Key path"
+  default = "./keys/region-nyc"
+}
